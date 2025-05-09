e8ed228e468a94b78d73cade1348cdcf847ced80
mathew kiwanuka <kiwamathew6@gmail.com>
2025-05-09 12:43:05 +0300

    imported the uuid and random in the models.py,  removed the created at field as well

--- mathew_kiwanuka/vendorapp/vendorsapp/models.py
+++ mathew_kiwanuka/vendorapp/vendorsapp/models.py
@@ -1,15 +1,18 @@
 from django.db import models
+import random
+import uuid
 
 # Create your models here.
 
-class Product(models.Model):   
+class Product(models.Model): 
+      
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     price = models.PositiveIntegerField()
     quantity = models.PositiveIntegerField()
     color = models.CharField(max_length=50)
     image = models.ImageField(upload_to='product_images/', null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.name

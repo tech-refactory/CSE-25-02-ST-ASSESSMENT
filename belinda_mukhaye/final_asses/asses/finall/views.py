73f7f206554ec21109921d85e397a255de743245
Mukhaye Belinda <namuzbelinda@gmail.com>
2025-05-09 19:41:05 +0300

    added to views, forms and templates

--- belinda_mukhaye/final_asses/asses/finall/views.py
+++ belinda_mukhaye/final_asses/asses/finall/views.py
@@ -4,18 +4,12 @@ from .forms import AddProductForm
 from django.contrib import messages
 # Create your views here.
 def index(request):
-    if request.method == 'POST':
-        products = Product.objects.all().order_by('-id')
-        form = AddProductForm(request.POST, request.FILES) 
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Product has been added successfully')
-            return redirect('/')
-        else:
-            messages.error(request, 'lnvalid field')
-    else:
-        form = AddProductForm()
-        return render(request, 'index.html', {'form':form})
+    products = Product.objects.all()  # define it unconditionally, or set a default
+    return render(request, 'index.html', {'products': products})
+
+    
+
+    
    # Include request.FILES for file upload
         
         

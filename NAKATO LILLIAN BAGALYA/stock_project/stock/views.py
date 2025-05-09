e18ef57ee507ffb205c26c40f8b2473ea57ccec1
Lillian Nakato <lillianashley2001@gmail.com>
2025-05-09 12:10:02 +0300

    added view for mystock

--- NAKATO LILLIAN BAGALYA/stock_project/stock/views.py
+++ NAKATO LILLIAN BAGALYA/stock_project/stock/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from .models import *
 
 # Create your views here.
+def mystock(request):
+    product = Product.objects.all().order_by('id')
+    return render(request, "mystock.html", {'products': product})
+    
+        
 
--- NAKATO LILLIAN BAGALYA/stock_project/stock_project/urls.py
+++ NAKATO LILLIAN BAGALYA/stock_project/stock_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from  stock import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.mystock, name='mystock'),
 ]

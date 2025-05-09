e4fef5cf796f16a04d6614bf6f0a1f678bdaf4b5
mathew kiwanuka <kiwamathew6@gmail.com>
2025-05-09 13:20:09 +0300

    validated my form in the views.py after a post method is called

--- mathew_kiwanuka/vendorapp/vendorsapp/views.py
+++ mathew_kiwanuka/vendorapp/vendorsapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 from django.contrib import messages
@@ -9,3 +9,13 @@ from django.contrib import messages
 def dashboard(request):
     products = Product.objects.order_by('-created_at')
     form = ProductForm()
+
+    
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Form has been successfully submitted.")
+            return redirect('dashboard')
+        else:
+            messages.error(request, "Please correct the errors below.")

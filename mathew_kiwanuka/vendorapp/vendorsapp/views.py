461974bf65afa1bf58d7ad9e4a9058eac6fb37f5
mathew kiwanuka <kiwamathew6@gmail.com>
2025-05-09 13:16:27 +0300

    instantiated my product from database by date created and also instantiated the product form

--- mathew_kiwanuka/vendorapp/vendorsapp/views.py
+++ mathew_kiwanuka/vendorapp/vendorsapp/views.py
@@ -4,3 +4,8 @@ from .forms import *
 from django.contrib import messages
 
 # Create your views here.
+
+
+def dashboard(request):
+    products = Product.objects.order_by('-created_at')
+    form = ProductForm()

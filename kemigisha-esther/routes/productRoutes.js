d729d47431aa14c755d764f34944ae933928b8fd
Kemigisha Esther <estarkemi@gmail.com>
2025-05-09 14:12:43 +0300

    added changes to my pug

--- kemigisha-esther/routes/productRoutes.js
+++ kemigisha-esther/routes/productRoutes.js
@@ -2,27 +2,29 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
+const Product = require('../models/Product');  // Make sure you import your Product model
 
-router.get("/product", async(req,res) => {
-    try {
-      const products = await Product.findOne({_id:req.params.id})
-      res.render("product",{products});
-    } catch (error){
-      res.status(400).send("unable to find this item in the db")
-    }
-    
-    })
-    
-    router.post("/product", async(req,res) =>{
-      try {
-        const { productName, category, price, quantity, color, image } = req.body;
-        const newProduct = new Product({ productName, category, price, quantity, color, image });
-    
-        await newProduct.save();
-        res.redirect("/product")
-      } catch (error) {
-        res.status(400).send("unable to find this item in the database")
-      }
-    })
-  
+// GET route to fetch all products
+router.get("/product", async (req, res) => {
+  try {
+    const products = await Product.find(); // Use find() to get all products
+    res.render("product", { products });   // Pass the products to the template
+  } catch (error) {
+    res.status(400).send("Unable to find items in the db");
+  }
+});
+
+// POST route to add a new product
+router.post("/product", async (req, res) => {
+  try {
+    const { productName, category, price, quantity, color, image } = req.body;
+    const newProduct = new Product({ productName, category, price, quantity, color, image });
+
+    await newProduct.save();
+    res.redirect("/product"); 
+  } catch (error) {
+    res.status(400).send("Unable to save this item to the database");
+  }
+});
+
 module.exports = router;

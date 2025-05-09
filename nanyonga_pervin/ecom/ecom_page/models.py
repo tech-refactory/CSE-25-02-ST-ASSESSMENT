700f84eedb19e61d17578c40bf68f2b9bf201c32
Pervin Nanyonga <pervincathie724@gmail.com>
2025-05-09 12:21:45 +0300

    Included tuples on choices and made migrations

--- nanyonga_pervin/ecom/ecom_page/models.py
+++ nanyonga_pervin/ecom/ecom_page/models.py
@@ -3,15 +3,15 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     CATEGORY_CHOICES = [
-        'Smart Phones', 'Smart Phones',
-        'Fashion', 'Fashion',
-        'Interior Design', 'Interior Design',
-        'Laptops', 'Laptops',
+        ('Smart Phones', 'Smart Phones'),
+        ('Fashion', 'Fashion'),
+        ('Interior Design', 'Interior Design'),
+        ('Laptops', 'Laptops'),
     ]
 
     product_name = models.CharField(max_length=100, verbose_name="Product Name", blank=False, null=False)
     category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, verbose_name="Category", blank=False, null=False)
-    price = models.IntegerField(max_digits=10,  verbose_name="Price", blank=False, null=False, default=0)
+    price = models.IntegerField( verbose_name="Price", blank=False, null=False, default=0)
     quantity = models.IntegerField(verbose_name="Quantity", blank=False, null=False, default=0)
     color = models.CharField(max_length=100, verbose_name="Color", blank=False, null=False)
     image = models.ImageField(verbose_name="Upload Image", blank=False, null=False)

937a1c78e2ec22178e0c4c460457f1004a548575
mathew kiwanuka <kiwamathew6@gmail.com>
2025-05-09 12:52:42 +0300

    registered the product model in the admin.py file

--- mathew_kiwanuka/vendorapp/vendorsapp/models.py
+++ mathew_kiwanuka/vendorapp/vendorsapp/models.py
@@ -5,7 +5,7 @@ import uuid
 # Create your models here.
 
 class Product(models.Model): 
-      
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     price = models.PositiveIntegerField()

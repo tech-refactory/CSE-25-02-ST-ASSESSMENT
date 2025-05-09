20311dcceefe8df9f30a156d57045b80c268e406
Lauryn Hope Tuhaise <laurynhope29@gmail.com>
2025-05-09 12:26:14 +0300

    created a templates folder

--- lauryn_hope_tuhaise/e_commerce_project/e_commerce/forms.py
+++ lauryn_hope_tuhaise/e_commerce_project/e_commerce/forms.py
@@ -1,3 +1,9 @@
 from django.forms import ModelForm
 from django import forms
 from .models import *
+
+class Product(ModelForm):
+    class Meta:
+
+        model = Product
+        fields='__all__'

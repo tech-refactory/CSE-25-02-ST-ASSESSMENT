0f6d5ab9dbccb3f2ea96a9665ad5934d20c3c144
Agenorwot patience <patienceagenorwot631@gmail.com>
2025-05-09 20:05:00 +0300

    had errors in terminal decided to commit at once

--- Patience_Agenorwot/vendors/vendors/urls.py
+++ Patience_Agenorwot/vendors/vendors/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from venapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.product_list, name= 'product_list'),
 ]
+
+

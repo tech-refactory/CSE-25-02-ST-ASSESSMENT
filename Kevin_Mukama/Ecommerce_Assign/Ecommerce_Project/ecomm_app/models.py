aa774a6fe61a24a90abf7593b74d26eecc871e1e
Kevin Mukama <kevinmukama16@gmail.com>
2025-05-09 12:36:25 +0300

    Made migrations

--- Kevin_Mukama/Ecommerce_Assign/Ecommerce_Project/ecomm_app/models.py
+++ Kevin_Mukama/Ecommerce_Assign/Ecommerce_Project/ecomm_app/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     price = models.IntegerField(blank = True)
     quantity = models.IntegerField(blank = True)
     color = models.CharField(max_length=20, blank = True)
-    image = models.ImageField(upload_to='Document/, 'blank=True)
+    image = models.ImageField(upload_to='photos/', blank = True)
 
     def __str__(self):
         return self.name

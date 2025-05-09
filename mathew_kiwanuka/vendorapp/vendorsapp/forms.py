94769af73c488c3f03d844c9c595b591be9af83b
mathew kiwanuka <kiwamathew6@gmail.com>
2025-05-09 15:24:38 +0300

    validated the name and category field using forms validation errors

--- mathew_kiwanuka/vendorapp/vendorsapp/forms.py
+++ mathew_kiwanuka/vendorapp/vendorsapp/forms.py
@@ -3,9 +3,41 @@ from django import forms
 from django.forms import ModelForm
 
 
-
 class ProductForm(ModelForm):
     class Meta:
         model = Product
 
         fields = "__all__"
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+
+        if len(name) < 2:
+            raise forms.ValidationError("Please use more letters.")
+        
+        for part in name:
+            if not part.isalpha():
+                raise forms.ValidationError("Names must only contain letters.")
+            if not part[0].isupper():
+                raise forms.ValidationError("Each name must start with a capital letter.")
+            
+            if not part[1:].islower():
+                raise forms.ValidationError("All letters after the first must be lowercase")
+
+    
+    def clean_category(self):
+        category = self.cleaned_data.get('category')
+
+        if len(category) < 2:
+            raise forms.ValidationError("Please use more letters.")
+        
+        for part in category:
+            if not part.isalpha():
+                raise forms.ValidationError("Category must only contain letters.")
+            if not part[0].isupper():
+                raise forms.ValidationError("Each name must start with a capital letter.")
+            
+            if not part[1:].islower():
+                raise forms.ValidationError("All letters after the first must be lowercase")
+        
+        

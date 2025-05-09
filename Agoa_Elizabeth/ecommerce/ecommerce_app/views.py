a2a99459fc93aa32e9184f8d695e19c9e4cad0a9
Agoa Elizabeth <elizabethagoa@gmail.com>
2025-05-09 14:08:57 +0300

    created a table

--- Agoa_Elizabeth/ecommerce/ecommerce_app/views.py
+++ Agoa_Elizabeth/ecommerce/ecommerce_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.db.models import Sum
 from .models import Product
 from .forms import ProductForm
 
@@ -7,7 +8,18 @@ from .forms import ProductForm
 # Create your views here.
 
 def homepage(request):
-    return render(request, "vendors.html")
+    total_sales = Product.objects.aggregate(Sum('price'))['price__sum'] or 0
+    total_orders = 150000000  # Replace with actual logic if needed
+    total_instock = Product.objects.filter(Quantity__gt=0).aggregate(Sum('price'))['price__sum'] or 0
+    total_outofstock = Product.objects.filter(Quantity=0).count()
+
+    context = {
+        'total_sales': total_sales,
+        'total_orders': total_orders,
+        'total_instock': total_instock,
+        'total_outofstock': total_outofstock,
+    }
+    return render(request, "vendors.html", context)
 
 def vendors_products(request):
     products = Product.objects.all()

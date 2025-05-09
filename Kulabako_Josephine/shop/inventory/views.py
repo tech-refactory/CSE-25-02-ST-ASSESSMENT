285836ae3f982c9aa23f8fe6310fdb41ac50acdb
kulabako josephine <josephinekentrhines@gmail.com>
2025-05-09 12:59:27 +0300

    views

--- Kulabako_Josephine/shop/inventory/views.py
+++ Kulabako_Josephine/shop/inventory/views.py
@@ -1,31 +1,34 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
+from django.db.models import Sum, F
+from django.contrib import messages
 from .models import Product
 from .forms import ProductForm
-from django.db.models import Sum
-from.models import *
 
 def product_dashboard(request):
+    success = False
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('product_dashboard')
+            success = True
+            form = ProductForm()  # reseting the form after successfully  being submitted
+        else:
+            messages.error(request, 'Form is not valid')
     else:
         form = ProductForm()
 
     products = Product.objects.all()
     total_sales = products.aggregate(total=Sum('price'))['total'] or 0
-    total_orders = products.aggregate(total=Sum('price'))['total'] or 0  # Adjust logic as needed
-    total_stock = products.aggregate(stock_value=Sum(models.F('price') * models.F('quantity')))['stock_value'] or 0
+    total_stock = products.aggregate(stock_value=Sum(F('price') * F('quantity')))['stock_value'] or 0
     out_of_stock = products.filter(quantity=0).count()
 
     context = {
         'form': form,
         'products': products,
         'total_sales': total_sales,
-        'total_orders': total_orders,
         'total_stock': total_stock,
         'out_of_stock': out_of_stock,
+        'success': success,
     }
-    return render(request, 'index', context)
-
+    return render(request, 'index.html', context)

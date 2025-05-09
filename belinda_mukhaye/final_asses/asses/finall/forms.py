4bd94516767ab29f06bbd7c9bba43ce4b2afee30
Mukhaye Belinda <namuzbelinda@gmail.com>
2025-05-09 12:20:36 +0300

    changes commited

---
+++ belinda_mukhaye/final_asses/asses/finall/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import *
+
+class AddProductForm(forms.ModelForm):
+    class Meta:
+        model =Product
+        fields = '__all__'
+
+       
+
+      
--- belinda_mukhaye/final_asses/asses/finall/views.py
+++ belinda_mukhaye/final_asses/asses/finall/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import *
 # Create your views here.
+from django.contrib import messages
+
+
+def product(requset):
+    if requset.method =='POST':
+        forms = AddProductForm(requset.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Form has been submitted successfully')
+            return redirect('/')
+        else:
+             messages.error(request, 'lnvalid field')
+        else:
+        form = AddProductForm()
+    return render(request, 'product.html', {'form': form})
+

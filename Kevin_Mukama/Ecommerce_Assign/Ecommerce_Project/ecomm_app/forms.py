65eb2e05a79e1422200e8b888e1d4e1654557f56
Kevin Mukama <kevinmukama16@gmail.com>
2025-05-09 12:25:13 +0300

    Created model forms

--- Kevin_Mukama/Ecommerce_Assign/Ecommerce_Project/ecomm_app/forms.py
+++ Kevin_Mukama/Ecommerce_Assign/Ecommerce_Project/ecomm_app/forms.py
@@ -1,3 +1,7 @@
 from django.forms import ModelForm
 from .models import *
 
+class AddProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
--- Kevin_Mukama/Ecommerce_Assign/Ecommerce_Project/ecomm_app/views.py
+++ Kevin_Mukama/Ecommerce_Assign/Ecommerce_Project/ecomm_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def addproductView(request):
+    return render(request, 'mainpage.html')

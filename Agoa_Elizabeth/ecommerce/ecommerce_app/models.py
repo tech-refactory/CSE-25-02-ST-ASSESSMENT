74d97a63967a246509e0205c2da64795001465e7
Agoa Elizabeth <elizabethagoa@gmail.com>
2025-05-09 12:46:58 +0300

    corrected models.py

--- Agoa_Elizabeth/ecommerce/ecommerce_app/models.py
+++ Agoa_Elizabeth/ecommerce/ecommerce_app/models.py
@@ -4,8 +4,13 @@ from django import forms
 # Create your models here.
 
 class Product(models.Model):
-   name = models.CharField(max_length=100)
+   name = models.CharField(max_length=255)
    category = models.CharField(max_length=100)
-   price = models.CharField(max_length=100)
-   Quantity = models.CharField(max_length=100)
-   color = models.CharField(max_length=100)
+   price = models.PositiveIntegerField(max_length=100)
+   Quantity = models.IntegerField(max_length=100)
+   color = models.CharField(max_length=50)
+   image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+
+def __str__(self):
+        return self.name

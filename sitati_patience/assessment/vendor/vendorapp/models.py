9d6beee1e985a4b98d6076886980bdcef79275c2
Sitati Patience <patiencesitati99@gmail.com>
2025-05-09 12:15:05 +0300

    worked on the models

--- sitati_patience/assessment/vendor/vendorapp/models.py
+++ sitati_patience/assessment/vendor/vendorapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    color = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self):
+        return self.product_name

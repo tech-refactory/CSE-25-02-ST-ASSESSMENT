3548459aa3a3eec54ae3566e586a2692f0d968df
Kemigisha Esther <estarkemi@gmail.com>
2025-05-09 13:15:03 +0300

    added a get route for the table

--- kemigisha-esther/routes/productRoutes.js
+++ kemigisha-esther/routes/productRoutes.js
@@ -5,5 +5,12 @@ router.get("/product", (req, res) => {
     res.render("product");
   });
 
-
+  router.get('/product', async (req, res) => {
+    try {
+      const products = await Product.find(); 
+      res.render('product', { products });   
+    } catch (err) {
+      res.status(500).send('Server error');
+    }
+  });
 module.exports = router;

cb0abae6931a5e2d53314be6a3aa4920304e861f
Sitati Patience <patiencesitati99@gmail.com>
2025-05-09 12:18:31 +0300

    added view in the views.py

--- sitati_patience/assessment/vendor/vendorapp/views.py
+++ sitati_patience/assessment/vendor/vendorapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProductForm
+from .models import Product
+from django.db.models import Sum
 
 # Create your views here.
+
+
+def dashboard(request):
+    form = ProductForm()
+    success = False
+
+   
+    return render(request, 'dashboard.html', context)

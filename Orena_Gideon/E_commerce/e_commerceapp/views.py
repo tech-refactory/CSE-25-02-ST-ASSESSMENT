fef16a29cf9dc848897dd8935644fb6a0b098dca
Orena Gideon <orenagedion2020@gmail.com>
2025-05-09 12:26:40 +0300

    I have set up my django views and connecting the route for the display of my form in an html template

--- Orena_Gideon/E_commerce/e_commerceapp/views.py
+++ Orena_Gideon/E_commerce/e_commerceapp/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 
 # A view for handling the e-commerce application.
 def index(request):
+    if request.method == 'POST':
+        data = request.POST
+
+        # Manually data from the post request
+        
     return render(request, 'index.html')  

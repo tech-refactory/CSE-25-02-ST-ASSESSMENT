26c495619c0e349bc02bda7c94fad027befc1ecd
Arishaba-Peruth <lorisperutha@gmail.com>
2025-05-09 15:02:25 +0300

    improved some models

--- Arishaba_Peruth/XPhones_ecommerce/phoneapp/models.py
+++ Arishaba_Peruth/XPhones_ecommerce/phoneapp/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255, blank=False)
-    category = models.CharField(max_length=255, blank=False, default="Uncategorized")
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    quantity = models.PositiveIntegerField(default=1)
-    color = models.CharField(max_length=100, blank=True)
+    category = models.CharField(max_length=255, blank=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(blank=False)
+    color = models.CharField(max_length=100, blank=False)
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
 
     def __str__(self):

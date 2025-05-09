178d0e9c2ce0f63d26a3962db202f44b362b8e0d
Lillian Nakato <lillianashley2001@gmail.com>
2025-05-09 11:54:41 +0300

    created model Product

--- NAKATO LILLIAN BAGALYA/stock_project/stock/models.py
+++ NAKATO LILLIAN BAGALYA/stock_project/stock/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=50, blank=False,null=False,unique=True,default="")
+    category = models.CharField(max_length=50, blank=False,null=False)
+    price =  models.FloatField(blank=False,null=False,default=0)
+    quantity = models.IntegerField(blank=False,null=False,default=0)
+    color = models.CharField(max_length=50,blank=False,null=False)
+
+
+    def __str__(self):
+        return self.product_name

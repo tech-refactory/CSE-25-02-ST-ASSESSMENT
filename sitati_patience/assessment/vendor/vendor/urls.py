b7e6c6649168f6b310942d5fe89c2e89b6fceebd
Sitati Patience <patiencesitati99@gmail.com>
2025-05-09 12:32:00 +0300

    added url path for the app

--- sitati_patience/assessment/vendor/vendor/urls.py
+++ sitati_patience/assessment/vendor/vendor/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from vendorapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', )
+    path('', views.dashboard, name="dashboard")
 ]

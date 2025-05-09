7d8dd4bf2f6d1eb417cc30697a4772563f27ad38
Arishaba-Peruth <lorisperutha@gmail.com>
2025-05-09 12:25:40 +0300

    created a url for my views

--- Arishaba_Peruth/XPhones_ecommerce/phoneapp/views.py
+++ Arishaba_Peruth/XPhones_ecommerce/phoneapp/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import *
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
 def Xphones(request):
-    
+    if request.method == 'POST':
+        form = AddproductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Product added successfully!")
+            return redirect('')
+    else:
+        form = AddproductForm()
     return render(request, 'index.html')

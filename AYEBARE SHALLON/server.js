cd322c64f7c0f13563f8ad78fd96e2a1ec6f522f
Ayebare Shallon <ayebareshallon123@gmail.com>
2025-05-09 12:51:25 +0300

    renaming files

--- AYEBARE SHALLON/server.js
+++ AYEBARE SHALLON/server.js
@@ -8,6 +8,10 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
+
+app.use(express.static('public'));
+const products = require('./routes/products');
 
 // MongoDB Connection
 if (!process.env.MONGODB_URI) {
@@ -19,6 +23,7 @@ mongoose.connect(process.env.MONGODB_URI)
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
+app.use('/api', productRoutes);
 app.get('/', (req, res) => {
   res.send('Server is running');
 });

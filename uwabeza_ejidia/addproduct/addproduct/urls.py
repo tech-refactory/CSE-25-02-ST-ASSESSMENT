9221423db54bc3e624526a63ffa42a4a8844923a
Uwabeza Ejidia <uwabeazejidia94@gmail.com>
2025-05-09 15:14:01 +0300

    modifying my admin and restarting the database

--- uwabeza_ejidia/addproduct/addproduct/urls.py
+++ uwabeza_ejidia/addproduct/addproduct/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from ecommerce import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.vendor_dashboard, name='vendor_dashboard'),
 ]

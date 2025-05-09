c7a7cc316d7204f7d4374c94ce37fd44963fbcee
retisha256 <retishanakiyemba202@gmail.com>
2025-05-09 12:25:03 +0300

    renamed id in models to product_id

--- Retisha_Nakiyemba/ecommerce/ecommerce_app/models.py
+++ Retisha_Nakiyemba/ecommerce/ecommerce_app/models.py
@@ -5,7 +5,7 @@ import uuid
 
 
 class Product(models.Model):
-    id=models.UUIDField(primary_key=True,default =uuid.uuid4,editable=False)
+    product_id=models.UUIDField(primary_key=True,default =uuid.uuid4,editable=False)
     name = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)

a20bf4c53f700186749232b6631274725211280a
Pervin Nanyonga <pervincathie724@gmail.com>
2025-05-09 15:11:37 +0300

    Modified models

--- nanyonga_pervin/ecom/ecom_page/models.py
+++ nanyonga_pervin/ecom/ecom_page/models.py
@@ -2,19 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    CATEGORY_CHOICES = [
-        ('Smart Phones', 'Smart Phones'),
-        ('Fashion', 'Fashion'),
-        ('Interior Design', 'Interior Design'),
-        ('Laptops', 'Laptops'),
-    ]
+    
 
     product_name = models.CharField(max_length=100, verbose_name="Product Name", blank=False, null=False)
-    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, verbose_name="Category", blank=False, null=False)
-    price = models.IntegerField( verbose_name="Price", blank=False, null=False, default=0)
-    quantity = models.IntegerField(verbose_name="Quantity", blank=False, null=False, default=0)
+    category = models.CharField(max_length=100, verbose_name="Category", blank=False, null=False)
+    price = models.IntegerField( verbose_name="Price", blank=False, null=False, )
+    quantity = models.IntegerField(verbose_name="Quantity", blank=False, null=False, )
     color = models.CharField(max_length=100, verbose_name="Color", blank=False, null=False)
-    image = models.ImageField(verbose_name="Upload Image", blank=False, null=False)
+    image = models.CharField(verbose_name="Upload Image", blank=False, null=False)
 
     def total_sales(self):
         if self.price and self.quantity:

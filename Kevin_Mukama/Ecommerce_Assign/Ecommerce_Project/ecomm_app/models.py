8b8ae47bd396c14e29df12db197ad0c1d61ccda5
Kevin Mukama <kevinmukama16@gmail.com>
2025-05-09 12:18:48 +0300

    Added model fields after starting my app

---
+++ Kevin_Mukama/Ecommerce_Assign/Ecommerce_Project/ecomm_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    id_no = models.IntegerField(blank = True)
+    name = models.CharField(max_length=80, blank = True)
+    category = models.CharField(max_length=80, blank = True)
+    price = models.IntegerField(blank = True)
+    quantity = models.IntegerField(blank = True)
+    color = models.CharField(max_length=20, blank = True)
+    image = models.ImageField(blank=False)
+
+    def __str__(self):
+        return self.name

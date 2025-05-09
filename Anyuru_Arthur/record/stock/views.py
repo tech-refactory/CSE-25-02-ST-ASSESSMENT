e82844c9f5049d531a185fb13bccbb075f348153
Mnemosyne173 <arthurctf7@gmail.com>
2025-05-09 12:54:10 +0300

    added form functionality, updatable database

--- Anyuru_Arthur/record/stock/views.py
+++ Anyuru_Arthur/record/stock/views.py
@@ -8,11 +8,20 @@ def homePage(request):
     total_amount = 0 
     for each in stock:
         total_amount += each.price
-
+    sales_form = addStock(request.POST)
+    if request.method == 'POST':
+        if sales_form.is_valid():
+            new_stock = sales_form.save(commit=False)
+            new_stock.name = stock.name
+            new_stock.category = stock.category
+            new_stock.price = stock.price
+            new_stock.quantity = stock.quantity
+            sales_form.save()
 
     context = {
+        'sales_form': sales_form,
         'stocks': stock,
-        'sales': "{;,}".format(total_amount),
+        'sales': "{:,}".format(total_amount),
     }
     return render(request, 'index.html', context)
 

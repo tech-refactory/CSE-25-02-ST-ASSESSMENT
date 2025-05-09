64fed00df35c00a24c1e3cde52baec8d2b863f4f
Lauryn Hope Tuhaise <laurynhope29@gmail.com>
2025-05-09 12:50:34 +0300

    added a uuid for my models

--- lauryn_hope_tuhaise/e_commerce_project/e_commerce/models.py
+++ lauryn_hope_tuhaise/e_commerce_project/e_commerce/models.py
@@ -5,11 +5,12 @@ import uuid
 # Create your models here.
 
 class Product(models.Model):
+    base_id = 645341
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    display_id = models.CharField(max_length=10, unique=True, editable=False)
+    product_id = models.CharField(max_length=10, unique=True, editable=False)
     product_name=models.CharField(max_length=50, blank=False, null=False)   
     category = models.CharField(max_length=100, blank=False, null=False)
-    price = models.PositiveIntegerField( blanck=False, null=False)
+    price = models.PositiveIntegerField( blank=False, null=False)
     quantity = models.PositiveIntegerField(blank=False, null=False)
     color = models.CharField(max_length=50, blank=False, null=False)
     image = models.ImageField(upload_to='products/', blank=False, null=False)
@@ -17,18 +18,10 @@ class Product(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
-        if not self.display_id:
-            # Generate a display ID like #645341
-            last_product = Product.objects.order_by('-created_at').first()
-            if last_product and last_product.display_id.startswith('#'):
-                try:
-                    last_id = int(last_product.display_id[1:])
-                    self.display_id = f"#{last_id + 1}"
-                except ValueError:
-                    self.display_id = f"#{645341}"
-            else:
-                self.display_id = f"#{645341}"
-        super(Product, self).save(*args, **kwargs)
+        if not self.product_id:
+            last = Product.objects.order_by('-product_id').first()
+            self.product_id = self.base_id + 1 if not last else last.product_id + 1
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.display_id} - {self.name}"
+        return f"{self.product_id} - {self.name}"
--- lauryn_hope_tuhaise/e_commerce_project/e_commerce/views.py
+++ lauryn_hope_tuhaise/e_commerce_project/e_commerce/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import *
+from django.contrib import messages
+from django.contrib.humanize.templatetags.humanize import intcomma
 
 # Create your views here.
+def index(request):
+    product = Product.objects.all()
+    
+    return render(request, 'index.html')

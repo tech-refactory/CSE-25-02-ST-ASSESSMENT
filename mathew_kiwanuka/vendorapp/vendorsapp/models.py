a6ea8073fd0362adbde3ba0606df37b02e47a439
mathew kiwanuka <kiwamathew6@gmail.com>
2025-05-09 12:23:27 +0300

    added the self method in models.py

--- mathew_kiwanuka/vendorapp/vendorsapp/models.py
+++ mathew_kiwanuka/vendorapp/vendorsapp/models.py
@@ -10,3 +10,6 @@ class Product(models.Model):
     color = models.CharField(max_length=50)
     image = models.ImageField(upload_to='product_images/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name

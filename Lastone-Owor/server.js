08460c9c29b7d6c100c745558ce345421eede9d2
Lastone <Oworilastone.com@gmail.com>
2025-05-09 13:33:37 +0300

    created the routes

--- Lastone-Owor/server.js
+++ Lastone-Owor/server.js
@@ -1,28 +1,18 @@
 require('dotenv').config(); // <--- Load .env variables first
 const express = require('express');
+const mongoose = require('mongoose');
+const productRoutes = require('./routes/products');
 const path = require('path');
-const mongoose = require("mongoose");
-const passport = require("passport");
-const expressSession = require("express-session");
 
 // instastiations
 const app = express();
 const PORT = 3002;
 
-console.log('MongoDB URI:', process.env.DATABASE);
-mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  
-  mongoose.connection
-    .on("open", () => {
-      console.log("Mongoose connection open");
-    })
-    .on("error", (err) => {
-      console.log(`Connection error: ${err.message}`);
-    });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // configurations
 app.set("view engine", "pug");  // specify the view engine
@@ -33,20 +23,8 @@ app.use(express.urlencoded({ extended: true })); // this helps to parse data to 
 app.use(express.static(path.join(__dirname, "public"))); // this helps to serve static files and specifes a folder for static files
 
 
-// Passport configuration
-app.use(expressSession({
-    secret: "my secret",
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false }
-}));
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(passport.initialize());
-app.use(passport.session());
-
+app.use('/', productRoutes);
 
 // bootstrapping the server
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));

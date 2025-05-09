1bc1210bda23a9bd588771b6ead5bf3acfcb26b3
Kevin Mukama <kevinmukama16@gmail.com>
2025-05-09 14:09:27 +0300

    Dynamically add db content to table

--- Kevin_Mukama/Ecommerce_Assign/Ecommerce_Project/ecomm_app/views.py
+++ Kevin_Mukama/Ecommerce_Assign/Ecommerce_Project/ecomm_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import *
+from .models import *
 
 # Create your views here.
 def addproductView(request):
@@ -7,8 +8,15 @@ def addproductView(request):
         form = AddProductForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('allstockPath')
+            all_products = Product.objects.all().order_by('-id')
+            return render(request, ".html", {'all_products': all_products})
+            return redirect('addproductPath')
     
     else:
         form = AddProductForm()
         return render(request, "mainpage.html", {"form":form})
+
+
+
+def cardsView(request):
+    total_revenue

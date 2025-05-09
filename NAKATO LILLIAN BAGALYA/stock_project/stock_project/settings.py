4660f2b340e4335770f30a273c57c0d0ef2c2855
Lillian Nakato <lillianashley2001@gmail.com>
2025-05-09 12:44:15 +0300

    added forms.py

--- NAKATO LILLIAN BAGALYA/stock_project/stock_project/settings.py
+++ NAKATO LILLIAN BAGALYA/stock_project/stock_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'stock',
     'crispy_forms',
-    
+    'crispy_bootstrap5'
+
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'

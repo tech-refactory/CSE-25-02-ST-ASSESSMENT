ed2bd7e9bdb00f28b92b5f4533a6954f399900b2
Ayebare Shallon <ayebareshallon123@gmail.com>
2025-05-09 12:19:30 +0300

    error handling in the database

--- AYEBARE SHALLON/server.js
+++ AYEBARE SHALLON/server.js
@@ -10,6 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
+if (!process.env.MONGODB_URI) {
+    console.error('MONGODB_URI is not defined in .env file');
+  process.exit(1); 
+}
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
